039bfdedefa3275ca86ca520c3dab953dd91745b
Kevin Do <kevind0501@gmail.com>
2022-01-25 10:45:28 -0800

    working await async

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { getPinballMachines } from './services/promise-me';
+
 export default function App() {
+  getPinballMachines();
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,7 +1,10 @@
-/**
- * TODO:
- */
+export async function getPinballMachines() {
+  const resp = await fetch('https://ponball.herokuapp.com/api/v1/machines');
 
+  const data = await resp.json();
+  console.log(data);
+  return data;
+}
 /**
  * TODO:
  */
